72dfdb83a8a509e6b9e30abf7b2c9002617f93e2
kleberfort <kleberfort@gmail.com>
2022-03-14 11:02:30 -0300

    Final do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -15,22 +13,47 @@ public class Main {
 
 
         curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Descrição curso Java");
+        curso1.setDescricao("curso Java");
         curso1.setCargaHoraria(8);
 
         curso2.setTitulo("Curso JS");
-        curso2.setDescricao("Descrição curso JS");
+        curso2.setDescricao("curso JS");
         curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria do java");
-        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setDescricao("mentoria java");
         mentoria.setData(LocalDate.now());
 
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        System.out.println("--------------------");
+        Dev devKleber = new Dev();
+        devKleber.setNome("Kleber Ferreira");
+        devKleber.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos Kleber: " + devKleber.getConteudosInscritos());
+        devKleber.progredir();
+        devKleber.progredir();
+        devKleber.progredir();
+
+        System.out.println("Conteúdos Incritos Kleber: " + devKleber.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Kleber: "+ devKleber.getConteudosConcluidos());
+        System.out.println("XP: " + devKleber.calcularTotalXp());
+
+        System.out.println("-------------------");
+
+/*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
         System.out.println(conteudo);
+  */
     }
 }
